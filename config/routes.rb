a81534e20a433e4f9499ee5f33aad701d57876c8
Dylan Ng <ngxinde@gmail.com>
2016-07-26 00:50:22 +0800

    Finish static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   resources :posts
-  root 'application#welcome'  # controller#method
+  root 'static_pages#home'  # controller#method
+
+  get 'static_pages/home'
+  get 'static_pages/about'
+  get 'static_pages/help'
 
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
